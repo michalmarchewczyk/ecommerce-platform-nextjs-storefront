39b60471fc0f600e6e8fb60145a9f19e12b2df7e
Michał Marchewczyk <dev@marchewczyk.eu>
2023-06-11 00:27:14 +0200

    Fix revalidation API route

--- app/api/revalidate/route.ts
+++ app/api/revalidate/route.ts
@@ -1,13 +1,10 @@
-import {
-  NextRequest,
-  NextResponse,
-  unstable_revalidatePath,
-} from 'next/server';
+import { revalidatePath } from 'next/cache';
+import { NextRequest, NextResponse } from 'next/server';
 
 export async function GET(request: NextRequest) {
   const path = request.nextUrl.searchParams.get('path');
   if (path) {
-    unstable_revalidatePath(path);
+    revalidatePath(path);
     return NextResponse.json({ revalidated: path });
   }
   return NextResponse.json({ revalidated: false });
